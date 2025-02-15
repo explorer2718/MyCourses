20a3f42d134bb41fbb5ae63e10bb4a12547288ec
Wang Wennan <124745109+explorer2718@users.noreply.github.com>
2025-02-15 22:14:02 +0800

    Add files via upload

---
+++ mathematics/Optimal_transport/Spherical OT/bin/OT_demo.bat
@@ -0,0 +1,2 @@
+OT.exe -source maxplanck.nf25k.m -target maxplanck.nf25k.sphere.m 
+OT.exe -source brain.obj -target brain.sphere.obj 
---
+++ mathematics/Optimal_transport/Spherical OT/bin/view_mesh.bat
@@ -0,0 +1,5 @@
+generalizedviewer.exe -mesh brain.obj
+generalizedviewer.exe -mesh maxplanck.nf25k.m -key e
+generalizedviewer.exe -mesh brain.sphere.obj
+generalizedviewer.exe -mesh maxplanck.nf25k.sphere.m -key e
+
